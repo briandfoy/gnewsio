fe46ad31230e8673d709e9f083ad1a356720c140
brian d foy <briandfoy@pobox.com>
2026-03-01 22:37:57 -0500

    initial import

---
+++ lib/GNews.pm
@@ -0,0 +1,65 @@
+package GNews;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+GNews - The GNews.io API
+
+=head1 SYNOPSIS
+
+	use GNews;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/gnews
+
+=head1 AUTHOR
+
+brian d foy, C<< <> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2026-2026, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
